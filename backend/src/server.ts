fc60fecd60fc0ef14aafa9ad3d3271013b3c19db
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-06-16 00:03:08 +0300

    Adding the commands to the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,15 @@
 import "dotenv/config";
 import { Env } from "@/config/Env";
 import app from "@/app";
+import bot from "@/bot/Bot";
+import { registerCommands } from "@/bot/BotCommands";
+
+registerCommands();
 
 app.listen(Env.PORT, () => {
   console.log(`Server running on port ${Env.PORT} [${Env.NODE_ENV}]`);
 });
+
+bot.launch();
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
